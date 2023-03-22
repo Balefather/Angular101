7d010a64452bc13a2d035034915d4ad08d6b2715
Bastian Krogh Grauenkjær <bkg@digizuite.com>
2023-03-22 23:49:04 +0100

    Det perfekte push

--- src/app/components/machine-detail/machine-detail.component.ts
+++ src/app/components/machine-detail/machine-detail.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Machine } from '../../model/machine';
+import { Part } from '../../model/part';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Observable } from 'rxjs';
 import { MachineService } from '../../services/machine.service';
+import { PartService } from '../../services/part.service';
 
 @Component({
   selector: 'app-machine-detail',
@@ -12,10 +14,13 @@ import { MachineService } from '../../services/machine.service';
 })
 export class MachineDetailComponent {
   @Input() machine: Machine | null;
+  partsToAdd: Part[] = [];
+  updateParts: boolean;
 
   constructor(
     private route: ActivatedRoute,
     private machineService: MachineService,
+    private partService: PartService,
     private location: Location
   ) { }
 
@@ -24,11 +29,53 @@ export class MachineDetailComponent {
       const machineID = Number(params.get('machineID'));
       this.getMachine();
     });
-
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  updateMode(){
+    if (this.updateParts) {
+      this.updateParts = false;
+    } else {
+      this.updateParts = true;
+    }
+  }
+
+  loadPartsNotInMachine(): void {
+    const parts = this.partService.getParts();
+    const machine = this.machine;
+    if (machine != null && machine.parts != null) {
+      parts.forEach(element => {
+        element.forEach(part => {
+          if (!machine.parts.some(mpart => mpart.partID === part.partID)) {
+            this.partsToAdd.push(part);
+          }
+        });
+      });
+    }
+  }
+  
+  
+  addPartToMachine(part: Part): void {
+    this.machine?.parts.push(part);
+    const index = this.partsToAdd.findIndex(p => p.partID === part.partID);
+    // or const index = this.partsToAdd.indexOf(part);
+    if (index !== -1) {
+      this.partsToAdd.splice(index, 1);
+    }
+  }
+
+  removePartFromMachine(part: Part): void {
+    this.partsToAdd.push(part);
+    if (this.machine != null && this.machine.parts != null) {
+      const index = this.machine.parts.findIndex(p => p.partID === part.partID);
+      // or const index = this.partsToAdd.indexOf(part);
+      if (index !== -1) {
+        this.machine?.parts.splice(index, 1);
+      }
+    }
   }
 
   save(): void {
@@ -40,8 +87,10 @@ export class MachineDetailComponent {
 
   getMachine(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.machineService.getMachine(id)
-      .subscribe(machine => this.machine = machine);
+    this.machineService.getMachine(id).subscribe(machine => {
+      this.machine = machine;
+      this.loadPartsNotInMachine();
+    });
   }
 
 }
